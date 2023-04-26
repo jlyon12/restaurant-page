ccb0c99cced022918e4198bb1c218f1bf72e9fb7
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-04-26 17:24:56 -0600

    Refactor home.js
      use functions to create similar elements

--- src/js/home.js
+++ src/js/home.js
@@ -4,96 +4,64 @@ import phoneImg from '../assets/images/phone.svg';
 
 const main = document.querySelector('.main');
 
-const createMainBox = () => {
-	const mainBox = document.createElement('div');
-	mainBox.classList.add('main-box');
-	const divOne = document.createElement('div');
-	mainBox.appendChild(divOne);
-	const divTwo = document.createElement('div');
-	const mainBoxTitle = document.createElement('h2');
-	mainBoxTitle.textContent = 'Coffee kept simple,';
-	const mainBoxText = document.createElement('p');
-	mainBoxText.textContent = 'and brewed with intention.';
-	divTwo.appendChild(mainBoxTitle);
-	divTwo.appendChild(mainBoxText);
-	const divThree = document.createElement('div');
-	mainBox.appendChild(divThree);
-	mainBox.appendChild(divOne);
-	mainBox.appendChild(divTwo);
-	mainBox.appendChild(divThree);
-	main.appendChild(mainBox);
+const createBox = (className, boxNumber) => {
+	const box = document.createElement('div');
+	box.classList.add(className);
+	const leftBorder = document.createElement('div');
+	const contentBox = document.createElement('div');
+	contentBox.setAttribute('id', `box-${boxNumber}`);
+	const rightBorder = document.createElement('div');
+	box.appendChild(leftBorder);
+	box.appendChild(contentBox);
+	box.appendChild(rightBorder);
+	main.appendChild(box);
 };
 
-const createInfoBox = () => {
-	const infoBox = document.createElement('div');
-	infoBox.classList.add('info-box');
-	const divOne = document.createElement('div');
-	infoBox.appendChild(divOne);
-	const divTwo = document.createElement('div');
-	const divTwoList = document.createElement('ul');
-	const listItemOne = document.createElement('li');
-	const listItemOneIcon = new Image();
-	listItemOneIcon.src = clockImg;
-	const listItemOneText = document.createElement('p');
-	listItemOneText.textContent = '7am - 5pm daily';
-	listItemOne.appendChild(listItemOneIcon);
-	listItemOne.appendChild(listItemOneText);
-	const listItemTwo = document.createElement('li');
-	const listItemTwoIcon = new Image();
-	listItemTwoIcon.src = locationImg;
-	const listItemTwoText = document.createElement('p');
-	listItemTwoText.textContent = '1071 Valencia St, San Francisco CA, 94110';
-	listItemTwo.appendChild(listItemTwoIcon);
-	listItemTwo.appendChild(listItemTwoText);
-	const listItemThree = document.createElement('li');
-	const listItemThreeIcon = new Image();
-	listItemThreeIcon.src = phoneImg;
-	const listItemThreeText = document.createElement('p');
-	listItemThreeText.textContent = '(415) 826-0607';
-	listItemThree.appendChild(listItemThreeIcon);
-	listItemThree.appendChild(listItemThreeText);
-	divTwoList.appendChild(listItemOne);
-	divTwoList.appendChild(listItemTwo);
-	divTwoList.appendChild(listItemThree);
-	divTwo.appendChild(divTwoList);
-	const divThree = document.createElement('div');
-	infoBox.appendChild(divThree);
-	infoBox.appendChild(divOne);
-	infoBox.appendChild(divTwo);
-	infoBox.appendChild(divThree);
-	main.appendChild(infoBox);
+const appendBox = (boxNumber, el, content) => {
+	const currentBox = document.getElementById(`box-${boxNumber}`);
+	const element = currentBox.appendChild(document.createElement(el));
+	element.textContent = content;
 };
 
-const createNavOne = () => {
-	const div = document.createElement('div');
-	div.classList.add('nav-arrow');
-	const text = document.createElement('p');
-	text.classList.add('arrowText');
-	text.textContent = 'learn more';
+const createListItem = (boxNumber, imgName, text) => {
+	const item = document.createElement('li');
+	const img = new Image();
+	img.src = imgName;
+	const itemText = document.createElement('p');
+	itemText.textContent = text;
+	item.appendChild(img);
+	item.appendChild(itemText);
+	const currentBox = document.getElementById(`box-${boxNumber}`);
+	const currentList = currentBox.querySelector('ul');
+	currentList.appendChild(item);
+};
+
+const createArrow = (className, arrowNumber, label) => {
+	const container = document.createElement('div');
+	container.classList.add('nav-arrow');
 	const arrow = document.createElement('div');
-	arrow.setAttribute('id', 'arrowBtn1');
-
-	div.appendChild(text);
-	div.appendChild(arrow);
-	main.appendChild(div);
-};
-
-const createNavTwo = () => {
-	const div = document.createElement('div');
-	div.classList.add('nav-arrow');
-	const arrow = document.createElement('div');
-	arrow.classList.add('hidden');
-	arrow.setAttribute('id', 'arrowBtn2');
-	div.appendChild(arrow);
-	main.appendChild(div);
+	arrow.setAttribute('id', `arrowBtn${arrowNumber}`);
+	if (label !== undefined) {
+		const text = document.createElement('p');
+		text.classList.add('arrowText');
+		text.textContent = label;
+		container.appendChild(text);
+	}
+	container.appendChild(arrow);
+	main.appendChild(container);
 };
 
 const loadHome = () => {
 	main.textContent = '';
-	createMainBox();
-	createNavOne();
-	createInfoBox();
-	createNavTwo();
+	createBox('main-box', 1);
+	appendBox(1, 'h2', 'Coffee kept simple,');
+	appendBox(1, 'p', 'and brewed with intention.');
+	createArrow('nav-arrow', 1, 'learn more');
+	createBox('info-box', 2);
+	appendBox(2, 'ul', null);
+	createListItem(2, clockImg, '7am - 5pm daily');
+	createListItem(2, locationImg, '1071 Valencia St, San Francisco CA, 94110');
+	createListItem(2, phoneImg, '(415) 826-0607');
+	createArrow('hidden', 2);
 };
-
 export default loadHome;
